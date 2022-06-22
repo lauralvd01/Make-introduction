edb9df8c769b70eb738fc9af88cd3563cc3196cc
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-06-22 11:47:14 +0200

    Version qui fonctionne pour l'exo 1

--- date.h
+++ date.h
@@ -1,10 +1,13 @@
-#ifndef DATE.H
-#define DATE.H
+#ifndef DATE_H
+#define DATE_H
 
 typedef struct _Date {
     int jour;
     int mois;
-    int année;
+    int annee;
 } Date;
+
+Date* create_date(int day, int month, int year);
+void print_date(Date *date);
 
 #endif
